86b599989c0a62b9915ac1a86dc126bd1cb2d87f
Daison Carino <daison12006013@gmail.com>
2022-04-26 13:32:07 +0800

    Update github action build

--- build-github-action.sh
+++ build-github-action.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# Example: ./build.sh develop
+# Example: ./build-ci.sh develop
 wget -c https://github.com/daison12006013/gorvel/archive/refs/heads/$1.tar.gz -O - | tar -xz
 rm -rf $1.tar.gz
 rm -rf gorvel/
 mv gorvel-$1 gorvel
-docker image rm -f daison12006013/gorvel
-docker build -t daison12006013/gorvel .
-docker push daison12006013/gorvel:latest
+docker build . --file Dockerfile --tag my-image-name:$(date +%s)
